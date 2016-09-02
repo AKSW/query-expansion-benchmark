353533a9bfe6392c5addcbe10a28112e20380830
emarx <emarx@live.com>
2016-09-02 14:34:34 +0200

    - changing running example to create the index before start to benchmark the questions

--- expansion.benchmark/src/main/java/org/aksw/queryexpansion/benchmark/answergeneration/example/RunningExampleEvaluation.java
+++ expansion.benchmark/src/main/java/org/aksw/queryexpansion/benchmark/answergeneration/example/RunningExampleEvaluation.java
@@ -7,18 +7,20 @@ import java.util.List;
 import org.aksw.openqa.qald.QuestionResult;
 import org.aksw.openqa.qald.schema.Dataset;
 import org.aksw.openqa.qald.schema.Question;
-import org.aksw.queryexpansion.benchmark.answergeneration.GlimmerQALDEvaluation;
+import org.aksw.queryexpansion.benchmark.answergeneration.BM25FQALDEvaluation;
 import org.aksw.queryexpansion.benchmark.answergeneration.QueryExpansionBenchmark;
+import org.aksw.queryexpansion.benchmark.answergeneration.glimmer.RDFIndex;
 
 public class RunningExampleEvaluation {
 	public static void main(String[] args) throws Exception {
 		// in this example we are going to evaluate expanded queries with different words
 		
-		URL QALDFileURL = GlimmerQALDEvaluation.class
+		URL QALDFileURL = BM25FQALDEvaluation.class
 				.getResource("/org/aksw/queryexpansion/benchmark/chalenges/qald-4_multilingual_test_questions_diffWords.xml");
 		Dataset questionDataset = QueryExpansionBenchmark.generateAnswers(new File(QALDFileURL
 				.toURI()));
-		List<Question> questions = questionDataset.getQuestion();
+		List<Question> questions = questionDataset.getQuestion();		
+		RDFIndex index = QueryExpansionBenchmark.getIndex();
 		
 		for(Question q : questions) {
 			org.aksw.openqa.qald.schema.String expandedQueryString = q.getString().get(0); // all questions must have at least one schema.String
@@ -27,13 +29,12 @@ public class RunningExampleEvaluation {
 			// Do the expansion here....
 			
 			String myNewQuery = ""; // put your new query here
-			List<String> result = QueryExpansionBenchmark.query(myNewQuery); 
+			List<String> result = QueryExpansionBenchmark.query(index, myNewQuery); 
 			
 			// now you can compare
 			QuestionResult questionResult = QueryExpansionBenchmark.benchmark(q.getId(), result);
 			
 			System.out.println(questionResult.toString()); // here we go...
-		}
-		
+		}		
 	}
 }
